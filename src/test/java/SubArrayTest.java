9e132a3cde0537d2711283c19c6425e736def3c2
fasp <mail@fabianspringer.com>
2025-01-20 20:30:53 +0100

    SubArrayTest added

--- src/test/java/SubArrayTest.java
+++ src/test/java/SubArrayTest.java
@@ -17,4 +17,34 @@ public class SubArrayTest {
         assertTrue(positivesErgebnis);
         assertFalse(negativesErgebnis);
     }
+
+    @Test
+    public void find2dSubarrayTest(){
+        int[][] array = {
+                {34, 7, 23, 32, 5, 62, 78},
+                {45, 12, 89, 56, 90, 11, 67},
+                {24, 88, 73, 54, 31, 19, 40},
+                {99, 21, 18, 76, 43, 55, 66},
+                {81, 39, 28, 14, 92, 33, 50},
+                {60, 25, 47, 13, 70, 85, 22}
+        };
+
+        int[][] subArray = {
+            {54, 31, 19},
+            {76, 43, 55},
+            {14, 92, 33}
+        };
+
+        int[][] notSubArray = {
+            {8, 9, 10},
+            {13, 14, 15},
+            {18, 19, 20}
+        };
+
+        boolean positivesErgebnis = SubArray.finde2dSubArray(array, subArray);
+        boolean negativesErgebnis = SubArray.finde2dSubArray(array, notSubArray);
+
+        assertTrue(positivesErgebnis);
+        assertFalse(negativesErgebnis);
+    }
 }
